5b1f2e1a2133f9cf1623ba9b76116e7bfa186365
keamoMichael <michaelkeamo@gmail.com>
2026-02-19 21:27:41 +0200

    fix: update Lenis options in SmoothScroll.tsx to resolve build error

--- app/components/SmoothScroll.tsx
+++ app/components/SmoothScroll.tsx
@@ -8,13 +8,14 @@ const SmoothScroll = () => {
         const lenis = new Lenis({
             duration: 1.2,
             easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-            direction: "vertical",
-            gestureDirection: "vertical",
-            smooth: true,
-            mouseMultiplier: 1,
-            smoothTouch: false,
+            orientation: "vertical",
+            gestureOrientation: "vertical",
+            smoothWheel: true,
+            wheelMultiplier: 1,
+            syncTouch: false,
             touchMultiplier: 2,
         });
+
 
         function raf(time) {
             lenis.raf(time);
